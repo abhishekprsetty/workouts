e4764a9169518a3a1429c626588645be6bd33ed6
abhishekprsetty <abhishekprsetty@gmail.com>
2018-03-26 14:27:31 +0530

    helloworld

---
+++ src/com/example/worksouts/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.worksouts;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/com/example/worksouts/HelloWorldScala.scala
@@ -0,0 +1,7 @@
+package com.example.worksouts
+
+object HelloWorldScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
